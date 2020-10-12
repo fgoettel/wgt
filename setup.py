f7855bdb245fcb951e6817ce578d96adb602ea18
fgoettel <fabian@.goettel@gmail.com>
2020-10-12 23:55:11 +0200

    v0.3.2

--- setup.py
+++ setup.py
@@ -33,6 +33,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/fgoettel/wgt",
-    version="0.3.1",
+    version="0.3.2",
     zip_safe=False,
 )
--- wgt/__init__.py
+++ wgt/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Fabian Göttel"""
 __email__ = "fabian.goettel@gmail.com"
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 from .lueftungsanlage import WGT  # noqa: F401
